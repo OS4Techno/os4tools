22fbe4c819210f9d841c506145ee320a309f48e3
MichelCaron1 <33993095+MichelCaron1@users.noreply.github.com>
2023-09-07 14:01:22 -0400

    ApplicationUserList

---
+++ ApplicationUserList.ps1
@@ -0,0 +1,24 @@
+<# 
+
+OS4 Techno, Services TI Inc.
+Septembre 2023
+
+Purpose: Get Azure Enterprise Application list with user
+Repository: https://github.com/OS4Techno/OS4Tools
+
+#>
+
+Connect-AzureAD
+
+Set-Location $HOME
+
+ If (Get-Item -Path .\ApplicationUserList.csv){Remove-Item .\ApplicationUserList}
+      
+ ForEach($_ in (Get-AzureADServicePrincipal -All $true))
+    {  
+     Get-AzureADServiceAppRoleAssignment -ObjectId $_.objectId | 
+     Select-Object ResourceDisplayName,PrincipalDisplayName, PrincipalType | 
+     Export-Csv -Path .\ApplicationUserList.csv -NoTypeInformation -Append -Encoding utf8
+    }
+
+ Disconnect-AzAccount
